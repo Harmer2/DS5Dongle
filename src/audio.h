f820eb49d4b7c1aaaa680473554c77f7dbe13b97
Harmer2 <maxim.huiskamp@student.uva.nl>
2026-05-10 14:54:12 +0200

    Update audio.h

--- src/audio.h
+++ src/audio.h
@@ -11,8 +11,6 @@ void audio_init();
 void audio_loop();
 void core1_entry();
 void set_headset(bool state);
-
-// --- FIX: Added the missing declaration so main.cpp can use it ---
 void set_state_data(const uint8_t* data, const uint8_t len);
 
 #endif //DS5_BRIDGE_AUDIO_H
