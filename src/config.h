002a854bf9ea6902b4d7a4a818b41522ff15f10a
Harmer2 <282962446+Harmer2@users.noreply.github.com>
2026-05-11 23:52:53 +0200

    Update config.h

--- src/config.h
+++ src/config.h
@@ -3,6 +3,8 @@
 
 #include <stdint.h>
 
+#define CONFIG_PROFILE_COUNT 8
+
 struct config_t {
     float haptics_gain;
     float speaker_volume;
@@ -18,5 +20,9 @@ void config_init(void);
 const config_t& get_config(void);
 void set_config(const config_t& cfg);
 void config_save(void);
+
+uint8_t profile_get_active(void);
+bool profile_load(uint8_t slot);
+bool profile_save(uint8_t slot);
 
 #endif // DS5_BRIDGE_CONFIG_H
