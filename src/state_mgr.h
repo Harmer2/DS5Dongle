e728582999792f3901d9b9907fedc7b82071b963
Harmer2 <282962446+Harmer2@users.noreply.github.com>
2026-06-04 17:31:45 +0200

    Update state_mgr.h

--- src/state_mgr.h
+++ src/state_mgr.h
@@ -6,5 +6,6 @@
 void state_init();
 void state_set(uint8_t *data, const uint8_t size);
 void state_update(const uint8_t *data, const uint8_t size);
+void state_update_from_game(const uint8_t *data, uint16_t size);
 
 #endif // STATE_MGR_H
