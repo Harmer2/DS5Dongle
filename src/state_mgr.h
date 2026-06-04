5d4737a2dd29acaebb6dae27a39316ffb5774042
Harmer2 <282962446+Harmer2@users.noreply.github.com>
2026-06-04 16:09:20 +0200

    Update state_mgr.h

--- src/state_mgr.h
+++ src/state_mgr.h
@@ -1,12 +1,10 @@
-//
-// Created by awalol on 2026/5/15.
-//
+#ifndef STATE_MGR_H
+#define STATE_MGR_H
 
-#ifndef DS5_BRIDGE_STATE_MGR_H
-#define DS5_BRIDGE_STATE_MGR_H
+#include <cstdint>  // <-- ADD THIS LINE TO FIX THE ERROR
 
 void state_init();
 void state_set(uint8_t *data, const uint8_t size);
 void state_update(const uint8_t *data, const uint8_t size);
 
-#endif //DS5_BRIDGE_STATE_MGR_H
+#endif // STATE_MGR_H
