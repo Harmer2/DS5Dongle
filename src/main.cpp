282142e0f2d2d016322c9f3c079274093a247a85
Harmer2 <282962446+Harmer2@users.noreply.github.com>
2026-06-04 16:59:22 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -158,6 +158,7 @@ void tud_hid_set_report_cb(uint8_t itf, uint8_t report_id, hid_report_type_t rep
                 outputData[2] = 0x10;
                 state_update(buffer + 1, bufsize - 1);
                 state_set(outputData + 3, 63);
+                bt_write(outputData, sizeof(outputData));
                 break;
             }
         }
