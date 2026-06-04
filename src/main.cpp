f503605476c4c7c0d20c3d01130ea534304e6270
Harmer2 <282962446+Harmer2@users.noreply.github.com>
2026-06-04 18:50:13 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -148,7 +148,7 @@ void tud_hid_set_report_cb(uint8_t itf, uint8_t report_id, hid_report_type_t rep
 
     if (report_id == 0) {
         switch (buffer[0]) {
-           case 0x02: {
+case 0x02: {
                 uint8_t outputData[78];
                 outputData[0] = 0x31;
                 outputData[1] = reportSeqCounter << 4;
@@ -157,8 +157,11 @@ void tud_hid_set_report_cb(uint8_t itf, uint8_t report_id, hid_report_type_t rep
                 }
                 outputData[2] = 0x10;
                 
+                // 1. Process and translate the incoming raw USB data into the Bluetooth cache
                 state_update(buffer + 1, bufsize - 1);
-                memcpy(outputData + 3, buffer + 1, bufsize - 1);
+                
+                // 2. Pull the correctly aligned Bluetooth layout out into your transmission packet
+                state_set(outputData + 3, 63);
                 
                 bt_write(outputData, sizeof(outputData));
                 break;
